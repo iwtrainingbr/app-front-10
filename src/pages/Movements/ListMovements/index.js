3b45021bc0cf0797a7c18b4df10c39d4b838e541
Alessandro Feitoza <alessandrofeitoza01@gmail.com>
2021-12-18 11:31:40 -0300

    Add sidebar

--- src/pages/Movements/ListMovements/index.js
+++ src/pages/Movements/ListMovements/index.js
@@ -1,9 +1,29 @@
+import {useNavigate} from "react-router-dom";
+
+import {Fab} from "@mui/material";
+import {Add} from "@mui/icons-material";
+
 export default function ListMovements () {
-   
-    return (
-    <div>
+  const navigate = useNavigate();
+
+  const actionButtonStyle = {
+    position: 'absolute',
+    right: 16,
+    bottom: 16
+  };
+
+  return (
+    <div style={{height: '100%'}}>
         <h1>Lista de Movimentos</h1>
+
+        <div>
+
+        </div>
+
+        <Fab onClick={() => navigate('/movimentacoes/add')} sx={actionButtonStyle} color="primary">
+          <Add/>
+        </Fab>
     </div>
-    )
-    
+  )
+
 }
