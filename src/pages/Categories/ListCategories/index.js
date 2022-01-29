14bcb45601850b1f639586181236cc0aea83ee40
hitalofrota <hitalofrota@gmail.com>
2022-01-29 10:34:37 -0300

    Adição de Itens Dinamicos na tela Listar Categories

--- src/pages/Categories/ListCategories/index.js
+++ src/pages/Categories/ListCategories/index.js
@@ -16,26 +16,16 @@ import Typography from '@mui/material/Typography';
 import FolderIcon from '@mui/icons-material/Folder';
 import DeleteIcon from '@mui/icons-material/Delete';
 import {useNavigate} from "react-router-dom";
+import {useState} from 'react';
 
-import {Fab} from "@mui/material";
+import {Fab, Card, CardContent} from "@mui/material";
 import {Add} from "@mui/icons-material";
-
-function generate(element) {
-  return [0, 1, 2].map((value) =>
-    React.cloneElement(element, {
-      key: value,
-    }),
-  );
-}
-
-const Demo = styled('div')(({ theme }) => ({
-  backgroundColor: theme.palette.background.paper,
-}));
 
 
 export default function Categories() {
   const [dense, setDense] = React.useState(false);
   const [secondary, setSecondary] = React.useState(false);
+  const [data, setData] = useState ([1,2,3,4])
   const navigate = useNavigate();
 
   const actionButtonStyle = {
@@ -44,39 +34,46 @@ export default function Categories() {
     bottom: 16
   };
 
-
+  const CategoryItem = () => {
+    return (
+        <ListItem
+          secondaryAction={
+            <IconButton edge="end" aria-label="delete">
+              <DeleteIcon />
+            </IconButton>
+          }
+        >
+          <ListItemAvatar>
+            <Avatar>
+              <FolderIcon />
+            </Avatar>
+          </ListItemAvatar>
+          <ListItemText
+            primary="Single-line item"
+            secondary={secondary ? 'Secondary text' : null}
+          />
+        </ListItem>
+    )
+  }
 
   return(
     <div>
-      <Grid container spacing={2}>
-        <Grid item xs={12} md={6}>
+      <Grid container spacing={2}> 
+        <Grid item xs={12} md={12}>
           <Typography sx={{ mt: 4, mb: 2 }} variant="h6" component="div">
             Categorias
           </Typography>
-          <hr/>
-          <Demo>
-            <List dense={dense}>
-              {generate(
-                <ListItem
-                  secondaryAction={
-                    <IconButton edge="end" aria-label="delete">
-                      <DeleteIcon />
-                    </IconButton>
-                  }
-                >
-                  <ListItemAvatar>
-                    <Avatar>
-                      <FolderIcon />
-                    </Avatar>
-                  </ListItemAvatar>
-                  <ListItemText
-                    primary="Single-line item"
-                    secondary={secondary ? 'Secondary text' : null}
-                  />
-                </ListItem>,
-              )}
-            </List>
-          </Demo>
+          <hr/> {/* Final da parte superior */}
+
+          <Card>
+            <CardContent>
+              <List dense={dense}>
+                {data.map(() => (<CategoryItem/>))}
+              </List>
+            </CardContent>
+          </Card> 
+
+
         </Grid>
       </Grid>
 
